aed0f1a54992647c216ef0d07d89879492dfcbf5
user <gallardoirg@gmail.com>
2024-10-14 17:08:47 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 72558354,
+    "boundingBox": {
+        "lx": 472947.3125,
+        "ly": 2154829.25,
+        "lz": 2245.71142578125,
+        "ux": 474139.3125,
+        "uy": 2156021.25,
+        "uz": 3437.71142578125
+    },
+    "tightBoundingBox": {
+        "lx": 472947.29835,
+        "ly": 2154829.1539,
+        "lz": 2245.711516796875,
+        "ux": 473817.38375,
+        "uy": 2156021.1934,
+        "uz": 2312.957916796875
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 10.323022842407227,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
